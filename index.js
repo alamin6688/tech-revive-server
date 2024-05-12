f2ec72b21ebbc4faf7f5a33d34bb2b1878c5815f
alamin6688 <alamin.kzs06@gmail.com>
2024-05-13 02:18:28 +0600

    API data added to the database

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nrlryfn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,28 +17,40 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db("techRevive").collection("services");
+
+
+    // Services releted api
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("tech revive is running!");
+});
 
-app.get('/', (req, res)=>{
-    res.send('tech revive is running!')
-})
-
-app.listen(port, () =>{
-    console.log(`Tech Revive server is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Tech Revive server is running on port: ${port}`);
+});
